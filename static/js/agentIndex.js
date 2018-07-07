ba8a085ae814c54184bdbc8a8223aaab91621983
maheshnadar <mahesh.nadar@wohlig.com>
2018-07-07 16:13:42 +0530

    changes

--- static/js/agentIndex.js
+++ static/js/agentIndex.js
@@ -24,37 +24,6 @@ $('#send_private_message').on('click', function () {
 
 
 var private_socket = io.connect('http://127.0.0.1:5000/private');
-// private_socket.on('new_private_message', function (msg) {
-//     // console.log("new_private_message")
-//     // alert(msg);
-//     console.log("new_private_message",msg);
-//     var add_tab_html = '<button class="tablinks" onclick="openCity(event, ' + msg['username'] + ')">' + msg[
-//         'username'] + '</button>'
-//     $('#chattab').append(add_tab_html)
-
-//     var add_chatwin_html = '<div id="' + msg['username'] + '" class="tabcontent">' +
-//         '<div id = "chat" class="">' +
-//         '<img src="" alt="Avatar" style="width:100%;">' +
-//         '<p>' + msg['message'] + '</p>' +
-//         '<span class="time-right">11:00</span>' +
-//         '</div>'+
-//         '<div style="position:absolute; bottom:1%">'+
-//         '<input class="mytext" type="text" placeholder="Type a message" id="private_message" style="bottom: 2%; width: 50%; margin-left: 1%;" />'+
-//         '<span class="glyphicon glyphicon-share-alt send-glyp"></span>'+
-//         '</div>' 
-//         ;
-//     console.log(add_chatwin_html)
-//     $('#chatwin').append(add_chatwin_html);
-
-// });
-
-// exp---------------------------------->
-var me = {};
-me.avatar =
-    "https://lh6.googleusercontent.com/-lr2nyjhhjXw/AAAAAAAAAAI/AAAAAAAARmE/MdtfUmC0M4s/photo.jpg?sz=48";
-
-var you = {};
-you.avatar = "https://a11.t26.net/taringa/avatares/9/1/2/F/7/8/Demon_King1/48x48_5C5.jpg";
 
 function formatAMPM(date) {
     var hours = date.getHours();
@@ -103,36 +72,240 @@ function formatAMPM(date) {
 
 // }
 
-// function resetChat() {
-//     $("ul").empty();
-// }
+var app = angular.module("Agent", []);
 
-// $(".mytext").on("keydown", function (e) {
-//     if (e.which == 13) {
-//         var text = $(this).val();
-//         if (text !== "") {
-//             insertChat("you", text);
-//             $(this).val('');
-//         }
-//     }
-// });
+app.controller("agentController", function ($scope) {
+    // $scope.users = [];
+    // $scope.chatHistory = [];
+    $scope.chatHistory = chathistory;
+    $scope.messages = [];
+    $scope.selectedUser = {};
+    $scope.toggleText = "break";
 
-// $('body > div > div > div:nth-child(2) > span').click(function () {
-//     $(".mytext").trigger({
-//         type: 'keydown',
-//         which: 13,
-//         keyCode: 13
-//     });
-// })
+    $scope.agentReplay={};
+    $scope.agentReplay.text="";
 
-//-- Clear Chat
-// resetChat();
+    $scope.getChat = function (user) {
 
-//-- Print Messages
-// insertChat("me",
-//     "Hi Abhishek Fulzele Chrome! Welcome to Ross-Simons live chat support. What can we help you with?", 0);
-// insertChat("you", "Hi, Pablo", 1500);
-// insertChat("me", "What would you like to talk about today?", 3500);
-// insertChat("you", "Tell me a joke", 7000);
-// insertChat("me", "Spaceman: Computer! Computer! Do we bring battery?!", 9500);
-// insertChat("you", "LOL", 12000);
+        console.log("change chat", user)
+        $scope.selectedUser = user;
+        // for (var i = 0; i < $scope.chatHistory.length; i++) {
+        //     var history = $scope.chatHistory[i][chat.username.toString()];
+        //     console.log("for llop", chat.username, history)
+        //     if ($scope.chatHistory[i][chat.username.toString()]) {
+        //         console.log("found messgae", $scope.chatHistory[i])
+        //         $scope.userChat = history;
+        //     }
+        // }
+    }
+
+    $scope.removeUser = function (user) {
+        console.log("remove user")
+        for (var i = 0; i < $scope.chatHistory.length; i++) {
+            if ($scope.chatHistory[i].user1 == user.user1) {
+
+                $scope.chatHistory.splice(i, 1);
+                console.log("remove array", $scope.chatHistory);
+                $scope.selectedUser = {};
+            }
+        }
+    }
+    private_socket.on('agent_new_chat', function (msg) {
+        console.log("agent_new_chat", msg)
+        // alert(msg);
+        console.log("agent_new_chat", msg);
+        console.log("first check", agentemail, msg.user2);
+        if (agentemail == msg.user2) {
+            // for (var i = 0; i < $scope.chatHistory.length; i++) {
+            //     if ($scope.chatHistory[i].user1 == msg.user_email) {
+            //         $scope.chatHistory[i].chatlist.push(msg)
+            //     }
+            // }
+            $scope.chatHistory.push(msg);
+        }
+        // if (msg == "No user available") {
+        //     console.log("chat history", $scope.chatHistory);
+        //     return
+        // } else {
+        //     $scope.users.push(msg);
+        //     console.log($scope.users, "messge");
+        //     var chat = {};
+        //     var name = msg.username.toString();
+        //     var time = new Date();
+        //     chat[name] = [];
+        //     chat[name].push({
+        //         'message': msg.message,
+        //         'username': msg.username,
+        //         'time': time,
+        //         'agent': msg.agent,
+        //         'from': 'user'
+        //     })
+        //     console.log("chat", chat);
+        //     $scope.chatHistory.push(chat);
+        //     console.log("chat history", $scope.chatHistory);
+        // }
+
+
+        // var add_tab_html = '<button class="tablinks" onclick="openCity(event, ' + msg['username'] + ')">' + msg[
+        //     'username'] + '</button>'
+        // $('#chattab').append(add_tab_html)
+
+        // var add_chatwin_html = '<div id="' + msg['username'] + '" class="tabcontent">' +
+        //     '<div id = "chat" class="">' +
+        //     '<img src="" alt="Avatar" style="width:100%;">' +
+        //     '<p>' + msg['message'] + '</p>' +
+        //     '<span class="time-right">11:00</span>' +
+        //     '</div>'+
+        //     '<div style="position:absolute; bottom:1%">'+
+        //     '<input class="mytext" type="text" placeholder="Type a message" id="private_message" style="bottom: 2%; width: 50%; margin-left: 1%;" />'+
+        //     '<span class="glyphicon glyphicon-share-alt send-glyp"></span>'+
+        //     '</div>' 
+        //     ;
+        // console.log(add_chatwin_html)
+        // $('#chatwin').append(add_chatwin_html);
+
+        $scope.$digest();
+
+    });
+    private_socket.on('agent_ongoing_chat', function (msg) {
+        // console.log("new_private_message")
+        // alert(msg);
+        $scope.agentReplay.text = null;
+        console.log("agent_ongoing_chat", msg);
+        console.log("first check", agentemail, msg.agent_email);
+        if (agentemail == msg.agent_email) {
+            for (var i = 0; i < $scope.chatHistory.length; i++) {
+                if ($scope.chatHistory[i].user1 == msg.user_email) {
+                    $scope.chatHistory[i].chatlist.push(msg)
+                }
+            }
+        }
+
+        // for (var i = 0; i < $scope.chatHistory.length; i++) {
+        //     var history = $scope.chatHistory[i][msg.username.toString()];
+        //     console.log("for new private llop", msg.username, history)
+        //     if ($scope.chatHistory[i][msg.username.toString()]) {
+        //         console.log("found messgae", $scope.chatHistory[i])
+        //         $scope.userChat = history;
+        //         var time = new Date();
+        //         $scope.chatHistory[i][msg.username.toString()].push({
+        //             'message': msg.message,
+        //             'username': msg.username,
+        //             'time': time,
+        //             'agent': msg.agent,
+        //             'from': (msg.type ? 'agent' : 'user')
+        //         })
+        //     }
+        // }
+
+
+
+
+        // var add_tab_html = '<button class="tablinks" onclick="openCity(event, ' + msg['username'] + ')">' + msg[
+        //     'username'] + '</button>'
+        // $('#chattab').append(add_tab_html)
+
+        // var add_chatwin_html = '<div id="' + msg['username'] + '" class="tabcontent">' +
+        //     '<div id = "chat" class="">' +
+        //     '<img src="" alt="Avatar" style="width:100%;">' +
+        //     '<p>' + msg['message'] + '</p>' +
+        //     '<span class="time-right">11:00</span>' +
+        //     '</div>'+
+        //     '<div style="position:absolute; bottom:1%">'+
+        //     '<input class="mytext" type="text" placeholder="Type a message" id="private_message" style="bottom: 2%; width: 50%; margin-left: 1%;" />'+
+        //     '<span class="glyphicon glyphicon-share-alt send-glyp"></span>'+
+        //     '</div>' 
+        //     ;
+        // console.log(add_chatwin_html)
+        // $('#chatwin').append(add_chatwin_html);
+
+        $scope.$digest();
+
+    });
+
+
+    private_socket.on('user_end_chat', function (endUser) {
+        //to end chat
+
+        console.log("inside user end chat", endUser);
+        for (var i = 0; i < $scope.chatHistory.length; i++) {
+            if ($scope.chatHistory[i].user1 == endUser.useremail) {
+                console.log("found user name", $scope.chatHistory[i]);
+                $scope.chatHistory[i].isChatEnd = true;
+                if ($scope.chatHistory[i].user1 == $scope.selectedUser.user1) {
+                    $scope.selectedUser.isChatEnd = true;
+                }
+                console.log($scope.chatHistory);
+            }
+        }
+        // console.log(msg);
+
+        $scope.$digest();
+
+    });
+    $scope.sendMessage = function (msg, user) {
+        if($scope.agentReplay.text !== ""){
+            console.log('inside send message', msg, user);
+            private_socket.emit('second_private_message', {
+                // "type": "agent",
+                // "agentname": data[0].agent,
+                // 'username': data[0].username,
+                // 'message': msg
+                agent_email: agentemail,
+                date: new Date(),
+                from_id: agentemail,
+                fromname: agentname,
+                message: msg,
+                to_id: user[1].to_id,
+                toname: user[1].toname,
+                type: "agent",
+                user_email: user[1].to_id,
+                user_details:{}
+            });
+        }
+        $scope.agentReplay.text = "";
+        console.log("Agent Msg Clear Input", $scope.agentReplay.text);
+    }
+
+    $scope.sendViaEnter = function ($event, msg, user) {
+        var keycode = $event.which || $event.keycode;
+        if (keycode === 13) {
+            $scope.sendMessage(msg, user);
+        }
+    }
+
+    $scope.break = function () {
+        var name = $('.agentname').text();
+        console.log(name);
+        private_socket.emit('break_message', {
+            "agentname": name,
+        });
+    }
+
+    $scope.changeButtonTxt = true;
+
+    $scope.$watch('changeButtonTxt', function () {
+        $scope.toggleText = $scope.changeButtonTxt ? 'Break' : 'Resume';
+    })
+
+    $scope.endChat = function (user) {
+        var name = $('.agentname').text();
+        console.log("end chat", user);
+        private_socket.emit('end_user_message', {
+            // "agentname": name,
+            // "type": "agent",
+            // "username": data[0].username,
+            agent_email: agentemail,
+            date: new Date(),
+            from_id: agentemail,
+            fromname: agentname,
+            message: "",
+            to_id: user[1].to_id,
+            toname: user[1].toname,
+            type: "agent",
+            user_email: user[1].to_id,
+            user_details:{}
+        });
+    }
+
+});
